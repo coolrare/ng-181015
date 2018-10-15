3101217dc3517528fb08340cee6ad2a1d97093bb
Will <doggy.huang@gmail.com>
2018-10-15 16:29:16 +0800

    示範 屬性繫結

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   sitename = 'Hello World'
+  sitelogo = '/assets/images/logo.png';
+  subtitle = '記載著 Will 在網路世界的學習心得與技術分享'
 
   constructor() { }
 
